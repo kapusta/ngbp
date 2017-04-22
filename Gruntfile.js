f267afae42699ab4966ccadd906b6628d068a0e1
Daniel Kapusta <daniel.kapusta@teamaol.com>
2017-04-21 20:59:29 -0400

    rimraf jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,5 @@
 module.exports = function (grunt) {
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-copy');
@@ -10,17 +9,10 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-babel');
 
   // if you simply run 'grunt' these default tasks will execute, IN THE ORDER THEY APPEAR!
-  grunt.registerTask('default', ['jshint', 'clean', 'babel', 'ngtemplates', 'concat', 'cssmin', 'copy']);
+  grunt.registerTask('default', ['clean', 'babel', 'ngtemplates', 'concat', 'cssmin', 'copy']);
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
-    jshint: {
-      files: ['./src/js/**/*.js', './src/components/**/*.js'],
-      options: {
-        jshintrc: '.jshintrc'
-      },
-    },
 
     clean: {
       options: {
